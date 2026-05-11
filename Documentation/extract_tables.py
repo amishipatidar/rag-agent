2841116102c4ef626ea303f49ae888ea5e2f94ba
amishipatidar <amishipatidar@gmail.com>
2026-05-11 15:24:42 +0530

    Remove Documentation folder for a cleaner repository

--- Documentation/extract_tables.py
+++
@@ -1,7 +0,0 @@
-import docx
-doc = docx.Document(r'c:\_My Data\RAGFlow\Documentation\RAGFlowDocumentation.docx')
-text = []
-for table in doc.tables:
-    for row in table.rows:
-        text.append(' | '.join([cell.text.replace('\n', ' ') for cell in row.cells]))
-open('RAGFlowDoc_tables.txt', 'w', encoding='utf-8').write('\n'.join(text))
